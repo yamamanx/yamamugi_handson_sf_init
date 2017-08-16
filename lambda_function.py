6f10158492555cb05eaa18ccbc398c6cf7a2c1a0
mitsuhiro-yamashita <yamamanx@gmail.com>
2017-08-16 20:32:10 +0900

    logger debug 可能にした

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,31 @@
 import logging
 import traceback
+import os
 
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
 
-def lambda_handler(event, context):
-    logger = logging.getLogger()
+logger = logging.getLogger()
+
+if log_level == 'ERROR':
+    logger.setLevel(logging.ERROR)
+elif log_level == 'DEBUG':
+    logger.setLevel(logging.DEBUG)
+else:
     logger.setLevel(logging.INFO)
 
+def lambda_handler(event, context):
+
     try:
+        logger.debug(event)
+
         phone_number = event.get('phone_number', '')
+        logger.debug(phone_number)
 
         if phone_number:
             icon = ':telephone_receiver:'
         else:
             icon = ':iphone:'
+        logger.debug(icon)
 
         to_slack_first = {
             'icon': icon
@@ -21,8 +34,9 @@ def lambda_handler(event, context):
             'to_kintone': event,
             'to_slack_first': to_slack_first
         }
-        return output
+        logger.debug(output)
 
+        return output
 
     except Exception as e:
         logger.error(traceback.format_exc())
